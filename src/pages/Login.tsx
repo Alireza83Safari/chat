a4ef1ad9fd297850cffcab0020e7ba9719a0bd2d
Alireza <alireza83safarii@gmail.com>
2024-02-08 20:45:42 +0330

    feat: create login and register page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { axiosInstance } from "../services/axios";
+import toast from "react-hot-toast";
 
 interface LoginData {
   username: string;
@@ -8,6 +9,7 @@ interface LoginData {
 }
 
 const Login: React.FC = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
   const [loginData, setLoginData] = useState<LoginData>({
     username: "",
@@ -21,65 +23,103 @@ const Login: React.FC = () => {
 
   const loginHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    try {
+      const data = await axiosInstance.post(`/auth/api/v1/login`, loginData);
 
-    const data = await axiosInstance.post(`/auth/api/v1/login`, loginData);
-
-    if (data.status === 200) {
-      localStorage.setItem("Authorization", data?.data?.token);
-      const expireTime = new Date(data?.data?.expiresAt);
-      document.cookie = `Authorization= ${data?.data?.token} ; expires=${expireTime}; secure; path=/; `;
-      navigate("/room");
+      if (data.status === 200) {
+        toast.success("login is successfully");
+        const expireTime = new Date(data?.data?.data?.expiresAt);
+        document.cookie = `Authorization= ${data?.data?.data?.token} ; expires=${expireTime}; secure; path=/; `;
+        navigate("/room");
+      }
+    } catch (error) {
+      setErrorMessage((error as any)?.response?.data?.message);
     }
   };
 
   return (
-    <div className="bg-white min-h-screen flex justify-center">
-      <div className="mt-20 shadow-2xl max-h-[23rem] py-5 px-6 rounded-lg">
-        <h1 className="text-[#0275FF] text-3xl font-semibold text-center">
-          Login
-        </h1>
+    <div className="flex justify-center items-center min-h-screen bg-white">
+      <div className="max-w-[40rem] sm:min-w-[28rem] min-w-[95vw] sm:px-0 px-4 py-4 rounded-lg shadow-2xl">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="text-center text-red-600 mt-4 text-sm">
+            {errorMessage}
+          </p>
+        </div>
 
-        <form className="mt-7" onSubmit={loginHandler}>
-          <div>
-            <label className="block font-semibold text-[#0275FF]">
-              Username
-            </label>
-            <input
-              type="text"
-              className="bg-white border border-[#0275FF] outline-none py-2 rounded-md min-w-[16rem] px-2"
-              value={loginData.username}
-              onChange={onChangeHandler}
-              name="username"
-            />
-          </div>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={loginHandler} method="POST">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  onChange={onChangeHandler}
+                  type="text"
+                  required
+                  className="block w-full px-1 bg-white rounded-md py-1.5 text-gray-900 shadow-sm border border-gray-300 placeholder:text-gray-400 focus:border-2 focus:border-indigo-600 outline-none sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-          <div className="mt-7">
-            <label className="block mt-4 font-semibold text-[#0275FF]">
-              Password
-            </label>
-            <input
-              type="password"
-              className="bg-white border border-[#0275FF] outline-none py-2 rounded-md min-w-[16rem] px-2"
-              value={loginData.password}
-              onChange={onChangeHandler}
-              name="password"
-            />
-          </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  onChange={onChangeHandler}
+                  autoComplete="current-password"
+                  required
+                  className="block w-full px-1 bg-white rounded-md py-1.5 text-gray-900 shadow-sm border border-gray-300 placeholder:text-gray-400 focus:border-2 focus:border-indigo-600 outline-none sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-          <button
-            type="submit"
-            className="bg-[#0275FF] text-white min-w-full mt-9 py-2 rounded-md"
-          >
-            Login
-          </button>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
 
-          <p className="text-xs mt-4 text-black-800 text-center">
-            you havent an account?{" "}
-            <Link to="/register" className="font-bold">
-              Register
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <Link
+              to="/register"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              create account
             </Link>
           </p>
-        </form>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,6 +1,7 @@
-import React, { ChangeEvent, FormEvent } from "react";
-import { api } from "../services/api";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { axiosInstance } from "../services/axios";
+import toast from "react-hot-toast";
 
 interface RegisterData {
   username: string;
@@ -8,6 +9,7 @@ interface RegisterData {
 }
 
 const Register: React.FC = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
   const [registrationData, setRegistrationData] = React.useState<RegisterData>({
     username: "",
@@ -21,55 +23,97 @@ const Register: React.FC = () => {
 
   const registerHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await fetch(`${api}auth/api/v1/register`, {
-      method: "POST",
-      body: JSON.stringify(registrationData),
-    });
-    console.log(response);
+    try {
+      const response = await axiosInstance.post(
+        `/auth/api/v1/register`,
+        registrationData
+      );
 
-    if (response.status === 200) {
-      navigate("/login");
+      if (response.status === 200) {
+        toast.success("register is successfully");
+        navigate("/login");
+      }
+    } catch (error) {
+      setErrorMessage((error as any)?.response?.data?.message);
     }
   };
 
   return (
-    <div className="bg-white min-h-screen flex justify-center">
-      <form
-        onSubmit={registerHandler}
-        className="mt-20 shadow-2xl max-h-[23rem] py-5 px-6 rounded-lg"
-      >
-        <h1 className="text-[#0275FF] text-3xl font-semibold text-center">
-          Register
-        </h1>
-
-        <div className="mt-7">
-          <label className="block font-semibold text-[#0275FF]">Username</label>
-          <input
-            type="text"
-            className="bg-white border border-[#0275FF] outline-none py-2 rounded-md min-w-[16rem] px-2"
-            value={registrationData.username}
-            onChange={onChangeHandler}
-            name="username"
-          />
+    <div className="flex justify-center items-center min-h-screen bg-white">
+      <div className="max-w-[40rem] sm:min-w-[28rem] min-w-[95vw] sm:px-0 px-4 py-4 rounded-lg shadow-2xl">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Create Account
+          </h2>
+          <p className="text-center text-red-600 mt-4 text-sm">
+            {errorMessage}
+          </p>
         </div>
 
-        <div className="mt-7">
-          <label className="block font-semibold text-[#0275FF]">Password</label>
-          <input
-            type="password"
-            className="bg-white border border-[#0275FF] outline-none py-2 rounded-md min-w-[16rem] px-2"
-            value={registrationData.password}
-            onChange={onChangeHandler}
-            name="password"
-          />
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={registerHandler} method="POST">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  onChange={onChangeHandler}
+                  type="text"
+                  required
+                  className="block w-full px-1 bg-white rounded-md py-1.5 text-gray-900 shadow-sm border border-gray-300 placeholder:text-gray-400 focus:border-2 focus:border-indigo-600 outline-none sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  onChange={onChangeHandler}
+                  autoComplete="current-password"
+                  required
+                  className="block w-full px-1 bg-white rounded-md py-1.5 text-gray-900 shadow-sm border border-gray-300 placeholder:text-gray-400 focus:border-2 focus:border-indigo-600 outline-none sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            have a account?{" "}
+            <Link
+              to="/login"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              login to account
+            </Link>
+          </p>
         </div>
-
-        <button className="bg-[#0275FF] text-white min-w-full mt-9 py-2 rounded-md">
-          Register
-        </button>
-        <p className="text-xs mt-4 text-black-800 text-center">you have an account? <Link to='/login' className="font-bold">Login</Link></p>
-
-      </form>
+      </div>
     </div>
   );
 };
