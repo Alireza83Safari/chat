479dcc6196f065b549861579468427d68d9b8563
Alireza <alireza83safarii@gmail.com>
2024-02-11 20:24:40 +0330

    feat: add onMouseLeave for close modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FaTimes } from "react-icons/fa";
 import { FaX } from "react-icons/fa6";
 
 interface ModalProps {
@@ -13,13 +12,16 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   return isOpen
     ? ReactDOM.createPortal(
         <div className="fixed inset-0 flex items-center justify-center bg-[#dddd]">
-          <div className="bg-white p-5 rounded-lg text-left overflow-hidden shadow-xl">
+          <div
+            className="bg-white p-5 rounded-lg text-left overflow-hidden shadow-xl relative"
+            onMouseLeave={onClose}
+          >
             <button
               onClick={onClose}
               type="button"
-              className="absolute top-0 right-0 mt-4 mr-4 text-gray-500 hover:text-gray-700 focus:outline-none"
+              className="absolute top-0 right-0 mt-2 mr-2 text-gray-500 hover:text-gray-700 focus:outline-none"
             >
-              <FaX className="text-[#ff0000] text-xl" />
+              <FaX className="text-[#ff0000]" />
             </button>
 
             <div className="mt-3 text-center sm:mt-0 sm:text-left">
